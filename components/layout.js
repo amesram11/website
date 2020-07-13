af62f0551efa89312225437c01e3e4575bb52dce
Saikat Chakrabarti <saikat1@gmail.com>
2020-07-13 14:54:25 -0700

    Add New Consensus logo

--- components/layout.js
+++ components/layout.js
@@ -4,46 +4,39 @@ import styled from '@emotion/styled'
 import { mobileBreakpoint } from './styles'
 import { slide as BurgerMenu } from 'react-burger-menu'
 
-const MenuBar = styled('div')`
+const DesktopMenu = styled('div')`
     margin: 0;
     padding: 0;
     background-color: #DB382F;
-    height: 95px;        
-`
-
-const DesktopMenu = styled('ul')`
-    text-align: center;
-    padding: 30px;
-    margin-left: 0;
-    margn-right: 0;
+    height: 95px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
 `
 const MenuItem = ({ children }) => {
     return (
-        <li css={css`
+        <a css={css`
             text-transform: uppercase;
+            color: #fff;
+            text-decoration: none;
             padding: 0 12px;
-            margin: 0 0 20px;
+            margin: 0;
             font-size: 19px;
             font-family: 'D-DIN-Exp', sans-serif;
             font-weight: 400;
-            display: inline-block;       
+            display: inline-block;
+            flex: 0 1 auto;      
         `}>
-            <a css={css`
-                display: inline-block;
-                color: #fff;
-                text-decoration: none;
-                padding: 0;
-                border-bottom: 5px solid transparent;
-            `}>
-                {children}
-            </a>
-        </li>
+            {children}
+        </a>
     )
 }
 
 const Logo = styled('div')`
     display: block;
-    margin: 0 auto;
+    margin-left: 2.5em;
+    margin-right: 2.5em;
     border: none;
     background: url("/images/logo-white.png") no-repeat 50% 50%;
     background-size: 100%;
@@ -57,15 +50,15 @@ const Logo = styled('div')`
 export default function Layout({ children }) {
     return (
         <div>
-            <MenuBar>
-                <DesktopMenu>
-                    <MenuItem>About</MenuItem>
-                    <MenuItem>Press</MenuItem>
-                    <MenuItem>Library</MenuItem>
-                    <MenuItem>Projects</MenuItem>
-                    <MenuItem>Blog</MenuItem>
-                </DesktopMenu>
-            </MenuBar>
+            <DesktopMenu>
+                <MenuItem>Memos</MenuItem>
+                <MenuItem>Projects</MenuItem>
+                <MenuItem>Library</MenuItem>
+                <MenuItem><Logo></Logo></MenuItem>
+                <MenuItem>Press</MenuItem>
+                <MenuItem>About</MenuItem>
+                <MenuItem>Donate</MenuItem>
+            </DesktopMenu>            
             {children}
         </div>
     )
