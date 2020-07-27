f90b3da48861b0d32bea72fae32b678e6a32c670
Saikat Chakrabarti <saikat1@gmail.com>
2020-07-27 12:05:59 -0700

    Add projects page

--- pages/projects.js
+++ pages/projects.js
@@ -68,14 +68,14 @@ const Projects = () => (
                     imageUrl='/images/gnd-14-pager-thumb.png'
                     projectUrl='/green-new-deal'
                 >
-                    Test
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </ProjectBlock>
                 <ProjectBlock
                     title='Pandemic Economics'
                     imageUrl='/images/gnd-14-pager-thumb.png'
                     projectUrl='/pandemic-economics'
                 >
-                    Test
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </ProjectBlock>
             </SingleColumnContent>
         </Section>
