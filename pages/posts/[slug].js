0a982f9cb04cdd7e6b1be906dcdcd60b50a6a7b3
Saikat Chakrabarti <saikat1@gmail.com>
2020-07-28 15:28:18 -0700

    Hook up backend

---
+++ pages/posts/[slug].js
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+import Layout from '../../shared/components/layout'
+import { SingleColumnContent, H1 }  from '../../shared/components/tags'
+import { getPostData, getAllPostIds } from '../../shared/lib/posts'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <SingleColumnContent>
+          <H1>
+              {postData.title}
+          </H1>
+          <div>
+              {postData.date}
+          </div>
+      </SingleColumnContent>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.slug)
+  return {
+    props: {
+      postData
+    }
+  }
+}
